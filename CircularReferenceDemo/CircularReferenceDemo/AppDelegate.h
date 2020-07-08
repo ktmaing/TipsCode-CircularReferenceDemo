b4673b3a5ffbe71959b23a14a56199a39206d641
ktmaing <1003271025@qq.com>
2020-07-08 11:23:37 +0800

    【create] 创建空项目

---
+++ CircularReferenceDemo/CircularReferenceDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CircularReferenceDemo
+//
+//  Created by king on 2020/7/8.
+//  Copyright © 2020 kingsely. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ CircularReferenceDemo/CircularReferenceDemo/MainVC.h
@@ -0,0 +1,17 @@
+//
+//  MainVC.h
+//  CircularReferenceDemo
+//
+//  Created by king on 2020/7/8.
+//  Copyright © 2020 kingsely. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MainVC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
